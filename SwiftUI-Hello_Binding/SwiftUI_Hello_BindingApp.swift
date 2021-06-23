2ab974f9e88426fc7466fe20cf562b3b7e4d7bd9
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 19:08:35 +0530

    Initial Commit

---
+++ SwiftUI-Hello_Binding/SwiftUI_Hello_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Hello_BindingApp.swift
+//  SwiftUI-Hello_Binding
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Hello_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
